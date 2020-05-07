ac4f9f6c8b1a611a1ed71eeb7dfb0a8a7aba21f4
Mateus Leandro da Silva Vieira <mateus.lsvieira@dominio.local>
2020-05-07 15:49:14 -0300

    dash

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,14 +6,30 @@ import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-  async function handleAddRepository(event: FormEvent<HTMLFormElement>): void {
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
     event.preventDefault();
 
     const response = await api.get(`repos/${newRepo}`);
+
+    const respository = response.data;
+
+    setRepositories([...repositories, respository]);
+    setNewRepo('');
   }
 
   return (
@@ -31,18 +47,20 @@ const Dashboard: React.FC = () => {
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/30327285?s=460&u=bf28763185a31f712933478ce5e921971cb4678c&v=4"
-            alt="Mateus Leandro"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
